5ce105d52d260f55a4732afbab046b309376f429
NavidKarami <43620005+NavidKarami@users.noreply.github.com>
2021-01-14 21:20:44 -0800

    Add files via upload

--- Codes/fft.py
+++ Codes/fft.py
@@ -1,24 +1,73 @@
 import os 
+import math
 import librosa
 from librosa import display
 from matplotlib import pyplot as plt 
-#import numba
+import scipy
+import matplotlib.pyplot as plt
+import numpy as np
 
-file_path = "Recording.wav"
-samples, sampling_rate = librosa.load(file_path, sr = None, mono = True, offset = 0.0, duration = None)
+def sound_wave():
+    file_path = "Recording.wav"
+    samples, sampling_rate = librosa.load(file_path, sr = None, mono = True, offset = 0.0, duration = None)
 
-len(samples), sampling_rate
-l = len(samples)
-r = sampling_rate
+    len(samples), sampling_rate
+    l = len(samples)
+    r = sampling_rate
 
-print("Number of samples is: %d" %l)     # Number of samples
-print("Number of samples per second is: %d " %r)    # We were capturing X amplitudes every second
+    print("Number of samples is: %d" %l)     # Number of samples
+    print("Number of samples per second is: %d " %r)    # We were capturing X amplitudes every second
 
-duration = len(samples)/sampling_rate   # How long was the audio
-print("Audio file was %.2f seconds long" %(duration))
+    duration = len(samples)/sampling_rate   # How long was the audio
+    print("Audio file was %.2f seconds long" %(duration))
 
-plt.figure()
-librosa.display.waveplot( y = samples, sr = sampling_rate)
-plt.xlabel("Time (seconds) -->")
-plt.ylabel("Amplitude")
-plt.show()
+    plt.figure()
+    librosa.display.waveplot(y = samples, sr = sampling_rate)
+    plt.xlabel("Time (seconds) -->")
+    plt.ylabel("Amplitude")
+    plt.show()
+
+def sound_fft(sound, rate):
+    n = len(sound)
+    T = 1/rate
+    yf = scipy.fft.fft(sound)
+    xf = np.linspace(0.0, 1.0/(2.0*T), int(n/2))
+    fig, ax = plt.subplots()
+    ax.plot(xf, 2.0/n * np.abs(yf[:n//2]))
+    plt.grid()
+    plt.xlabel("Frequency")
+    plt.ylabel("Magnitude")
+    return plt.show()
+
+def sample():
+    samples = 100
+    f = 3
+    x = np.arange(samples)
+    y1 = np.sin(2*np.pi*f * (x/samples))
+    plt.figure()
+    plt.stem(x,y1, 'r', )
+    plt.plot(x,y1)
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.show()
+
+    n = len(y1)
+    T = 1/100
+    yf = scipy.fft.fft(y1)
+    xf = np.linspace(0.0, 1.0/(2.0*T), int(n/2))
+    fig, ax = plt.subplots()
+    ax.plot(xf, 2.0/n * np.abs(yf[:n//2]))
+    plt.grid()
+    plt.xlabel("Freq")
+    plt.ylabel("Mag")
+    plt.show()
+
+
+def main():
+
+    file_path = "Recording.wav"
+    samples, sampling_rate = librosa.load(file_path, sr = None, mono = True, offset = 0.0, duration = None)
+    sound_fft(samples, sampling_rate)
+
+if __name__ == '__main__':
+    main()
