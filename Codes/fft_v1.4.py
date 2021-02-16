f601126da2b6db83adeb3a58082451478ce04232
NavidKarami <43620005+NavidKarami@users.noreply.github.com>
2021-02-16 00:06:19 -0800

    Added peak_finder function and got rid of the unwanted "x"

--- Codes/fft_v1.4.py
+++ Codes/fft_v1.4.py
@@ -16,27 +16,56 @@ from scipy.signal import argrelextrema
 from scipy.signal import argrelmax
 import scipy.io.wavfile as wavfile
 
+#This function can find the magnitude of the peaks
+def peak_finder(peaks, fft, length):
+#init the arrays
+    h = []
+    q = []   
+    for k in range (0, length):
+        q[k] = q.append(0)
+        h[k] = h.append(0)
+#find the magnitude of the peaks
+    for i in range (0, len(peaks)):
+        q[i] = fft[peaks[i]]
+        h[i] = q[i]
+#sort the magnitudes 
+    q.sort(reverse = True)
+    a = q[0] #Mag of the 1st highest peak
+    print("In peak_finder function Mag of the 1st highest peak is:", a)
+
 def sound_fft(sound, rate):
 
     fs_rate, signal = wavfile.read("dn2.wav")
 
-    n = len(sound)  #How long is the file
+    n = len(sound)  #How long is the audio file
     T = 1/rate      #Sampling interval in time
 
-    fft = scipy.fft.fft(sound)   # Do FFT
+    fft = scipy.fft.fft(sound)   #Do FFT
     fft = abs(fft)    #Get rid of the imaginary part
-
-    print(fft[393])
-    fft = np.array(fft)
+    #print(fft[393])
+    fft = np.array(fft) #Performing FFT. We could use scipy library too
 
     #Find the highest peak and print it
-    peaks,_ = find_peaks(fft, distance=30, height=10) #Find the peaks (peaks > 10)
+    fft_one_side = np.abs(fft[:n//2])
+    #peaks,_ = find_peaks(fft, distance=80, height=10) #Find the peaks (peaks > 10)
+    peaks,_ = find_peaks(fft_one_side, distance=80, height=10) #Got rid of the unwanted X
     peak1 = np.argmax(fft)
-    print("Highest Peak is: ", peak1)
+   
+    print("Highest Peak is:", peak1)
+    print("The highest peak magnitude is:", fft[peak1])
+   
+    length = len(peaks)
+    peak_finder(peaks, fft, length)
+
+    #This is a test
+    prominences = peak_prominences(fft, peaks, wlen=25)[0]
+    #print(prominences)
+    #print(fft[peaks])  #Print Magnitude of the peaks
 
     xf = np.linspace(0.0, 1.0/(2.0*T), int(n/2))
     #xf = np.linspace(0.0, T*rate, int(n/2))
     one_side = np.abs(fft[:n//2])
+    #peak_one_side = np.abs(peaks[:n//2])
     #print(one_side)
     """
     fig, ax = plt.subplots()
@@ -52,11 +81,10 @@ def sound_fft(sound, rate):
     plt.xlabel("Frequency")
     plt.ylabel("Magnitude")
     plt.show()
-
     #return plt.show()
 
 def main():
-    file_path = "dn2.wav"
+    file_path = "dn3.wav"
     samples, sampling_rate = librosa.load(file_path, sr = None, mono = True, offset = 0.0, duration = None)
     sound_fft(samples, sampling_rate)
 
