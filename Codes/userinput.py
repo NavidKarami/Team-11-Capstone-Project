717be20c4cc1644cc26dd8485659b9f760782e72
NavidKarami <43620005+NavidKarami@users.noreply.github.com>
2021-03-02 22:02:07 -0800

    New user module functioning
    03022021, New polished pin code

--- Codes/userinput.py
+++ Codes/userinput.py
@@ -5,59 +5,77 @@ from array import *
 import string
 import numpy as np  
 
-###########################################
-#fix the pins, allow 000x because it doesn't right now
-
 class user:
     #There is a one-to-one relationship between the name and pin. name_index = pin_index for each user
     name = ["uzi", "navid", "danny", "tamarr"]
-    pin = [2218, 1000, 9999, 1234]
-    i = len(name) 
+    pin = ['2218', '1000', '9999', '1234']
+    i = 0 
     len_name = len(name)  #Len of the name array
     count = 0 #keeps track of user attempts entering pin
     
 #sets the amount of registered users for database
 size = user.i + 3
 
-def NewUser():
-#this append might be wrong im not sure
-    if (user.i < size):
-        user.pin.append("")
-        
-#following code asks user to create a 4 digit pin, save as a string
-        while True:
-            user.pin[user.i] = input("\nSet your 4 digit pin: ")
-            x = str(user.pin[user.i])
-            #print("\nYou entered:", user.pin[user.i])
-        
-#checks if the pin is 4 integers long, if it is then you create a username  
-            if (len(x) == 4) and (x.isdigit()):
-                NewUser_name()
+def NEWUSER():
+    #New User Module 
+    #Ask the user to type the name
+    while True:
+        print("Since you are new into our system")
+        name = input("Enter your name\n")
+        quit = True 
+        for un in user.name:
+            if(name == un):
+                print("There is aleaday a username in the list")
+                print("Please input your name again")
+                quit = False
                 break
-#if it isn't you must keep trying until you meet the requirements
-            else:
-                print("Error. Try again.")
-    else:
-        print("can't register new users")
+        if(quit):
+            break
 
+    #Store the username in the system 
+    user.name.append(name)
+    print(user.name)
 
-def NewUser_name():
-#append is wrong not sure why
-#need to check that username doesn't match an already active username#########
-    user.name.append("")
-    user.name[user.i] = input("Choose a username. Must contains letters and numbers only: ")
+    #Ask the user for a four digit pin code 
+    #if the four-digit wasn't entered properly, Ask the user agian 
+    while True:
+        #check the length and numbers only 
+        pinnum = (input("Please enter your four-digit pincode\n"))
+        #print(pinnum)
+        length = len(pinnum)
+        print(length)
+        # Known: won't if you put in and 0000 within the pincode
+        if length !=4 or pinnum in user.pin:
+        #if(length != 4):
+            print("Invaild pincode, please try again!")
+            print("The pincode is being used")
+        else:            
+           break
 
-#not sure what the code below is for...###################################
-    if (user.name[user.i].isdigit()):
-        raise ValueError("The user name must contain letters and numbers.")
-        user.name[user.i] = str(user.name[user.i])
-        user.i += 1
-########################################################################
+    print("This is the pincode: ", pinnum)
+    #print(pinnum)
+    
+    while True:
+        #Check if we have the same pincode 
+        print("It's time to confirm your pincode")
+        pinnum2 = (input("Please enter your four-digit pincode\n"))
+        #print(pinnum2)
+        length2 = len(pinnum2)
+        #Confirm the four-digit pincode (Compare) 
+        if(pinnum != pinnum2):
+            print("The confirm pincode does not matched please try again")
+        else:
+            break 
 
-#to increment the array so we don't overwrite any users
-    user.i += 1
+    #Store the pincode in the system
+    user.pin.append(pinnum)
+    print(user.pin)
+    
+'''
+* To check if an item is in a list, the "in" operator can be used 
+* The in operator is alos used to determine whether or not a string is a substring of another string 
+'''
 
-#We do not need this at this time. The program only checks for the pin. No username comparison needed for current users
 """
 def currentUser():
     print("Welcome Current User.\n") 
@@ -77,6 +95,8 @@ def currentUser():
 
 #def current_user_pin(name, index):
 def current_user_pin():
+    user.len_name = len(user.pin)
+    print("checking:", user.len_name)
     temp_pin = input("Enter your pin:")
     #check the pin to match index. check name. Double checking is good
     for k in range(0, user.len_name):
@@ -84,7 +104,8 @@ def current_user_pin():
             print("Welcome: ", user.name[k])
             user.count = 0
             print("We go to FFT from here.")
-            exit(0)
+            #exit(0)
+            main() #It
 
     print("invalid. Try again.")
     user.count = user.count + 1 #inc and check to see if they atmp three times
@@ -93,7 +114,7 @@ def current_user_pin():
         time.sleep(10) #sleep
         main()
     current_user_pin() #call the pin function again
-
+    
 #It prints out the current user info. It is a test module. Not working at this time
 def userPrint():
 #testing################################################
@@ -126,7 +147,7 @@ def main():
         print("[1]: New user\n[2]: Current user\n[3]: TESTING PHASE (View User Info)\n[4]: Exit")
         select = input("Please select from the options above: ")
         if (select == '1'):
-            NewUser()
+            NEWUSER()
         elif (select == '2'):
             current_user_pin()
         elif (select == '3'):
