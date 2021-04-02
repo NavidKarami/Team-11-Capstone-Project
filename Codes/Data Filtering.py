de62f332efd8911e846d381d1fdd61ef29cf06cf
wdanny69 <39199279+wdanny69@users.noreply.github.com>
2021-04-01 19:53:19 -0700

    Add files via upload

---
+++ Codes/Data Filtering.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+#testing purposes, sample values to filter
+#cases checked, values close together within given tolernace range
+#duplicates, values within tolerance range at different points in the data (one at beginning/end)
+FreqData = [54,10,10,10,13,40,37,50]
+
+tolerance = 5 #bounds in which other values can not be within
+FilteredFreqData = []
+for Freq in FreqData: #scans through all values taken and filters any extra peaks within some tolerance range
+    if len(FilteredFreqData) == 0: #stores first and highest peak value
+        FilteredFreqData.append(Freq)
+        continue
+    
+    print(Freq, ' = Freq') #testing purposes, see current iteration
+    
+    #if the current iteration is within the bounds of any of the values then move to the next iteration
+    if any((Freq2-tolerance)<=Freq<=(Freq2+tolerance) for Freq2 in FilteredFreqData): 
+        continue
+
+    if Freq in FilteredFreqData: #avoid duplicate values
+        continue
+    FilteredFreqData.append(Freq)
+    
+#testing purposes, see final output
+print(FilteredFreqData)
+print(len(FreqData))
