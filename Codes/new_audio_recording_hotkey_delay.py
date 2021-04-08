4b7e57aaa57a50d545bd511ef6a1a0c55535e5ea
NavidKarami <43620005+NavidKarami@users.noreply.github.com>
2021-04-07 20:35:25 -0700

    Add files via upload

---
+++ Codes/new_audio_recording_hotkey_delay.py
@@ -0,0 +1,140 @@
+from sys import byteorder
+from array import array
+from struct import pack
+import time as t
+import pyaudio
+import wave
+
+THRESHOLD = 500
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+RATE = 44100
+
+def is_silent(snd_data):
+    "Returns 'True' if below the 'silent' threshold"
+    return max(snd_data) < THRESHOLD
+
+def normalize(snd_data):
+    "Average the volume out"
+    MAXIMUM = 16384
+    times = float(MAXIMUM)/max(abs(i) for i in snd_data)
+
+    r = array('h')
+    for i in snd_data:
+        r.append(int(i*times))
+    return r
+
+def trim(snd_data):
+    "Trim the blank spots at the start and end"
+    def _trim(snd_data):
+        snd_started = False
+        r = array('h')
+
+        for i in snd_data:
+            if not snd_started and abs(i)>THRESHOLD:
+                snd_started = True
+                r.append(i)
+
+            elif snd_started:
+                r.append(i)
+        return r
+
+    # Trim to the left
+    snd_data = _trim(snd_data)
+
+    # Trim to the right
+    snd_data.reverse()
+    snd_data = _trim(snd_data)
+    snd_data.reverse()
+    return snd_data
+
+def add_silence(snd_data, seconds):
+    "Add silence to the start and end of 'snd_data' of length 'seconds' (float)"
+    r = array('h', [0 for i in range(int(seconds*RATE))])
+    r.extend(snd_data)
+    r.extend([0 for i in range(int(seconds*RATE))])
+    return r
+
+def record():
+    """
+    Record a word or words from the microphone and 
+    return the data as an array of signed shorts.
+    Normalizes the audio, trims silence from the 
+    start and end, and pads with 0.5 seconds of 
+    blank sound to make sure VLC et al can play 
+    it without getting chopped off.
+    """
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT, channels=1, rate=RATE,
+        input=True, output=True,
+        frames_per_buffer=CHUNK_SIZE)
+
+    num_silent = 0
+    snd_started = False
+
+    r = array('h')
+    stopwatch(1)
+    print("Speak now")
+    while 1:
+        # little endian, signed short
+        snd_data = array('h', stream.read(CHUNK_SIZE))
+        if byteorder == 'big':
+            snd_data.byteswap()
+        r.extend(snd_data)
+
+        silent = is_silent(snd_data)
+
+        if silent and snd_started:
+            num_silent += 1
+        elif not silent and not snd_started:
+            snd_started = True
+
+        if snd_started and num_silent > 30:
+            break
+
+    sample_width = p.get_sample_size(FORMAT)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    r = normalize(r)
+    #r = trim(r)
+    #r = add_silence(r, 0.1)
+    return sample_width, r
+
+def record_to_file(path):
+    "Records from the microphone and outputs the resulting data to 'path'"
+    sample_width, data = record()
+    data = pack('<' + ('h'*len(data)), *data)
+
+    wf = wave.open(path, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(sample_width)
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
+
+## Countdown function starts here
+def stopwatch(sec):
+    while sec:
+        minn, secc = divmod(sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(minn, secc)
+        print(timeformat, end='\r')
+        t.sleep(1)
+        sec -= 1
+    #print('Goodbye!\n')
+## calling stopwatch function
+
+if __name__ == '__main__':
+    
+    u_input = str("k")
+    while ((u_input != "r") and (u_input != "e")):
+        print("You can start recording your voice by pressing 'r' on your keyboard. If you wish to exit press 'e'.")
+        u_input = input()
+    
+    print("please speak a word into the microphone after the displayed delay")
+    record_to_file('demo1.wav')
+    print("done - result written to demo1.wav")
+    print("please repeat your word into the microphone")
+    record_to_file('demo2.wav')
+    print("done - result written to demo2.wav")
